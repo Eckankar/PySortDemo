110bdaeaf62ce6b09fded12eb03acede2c4d51a8
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-12 20:51:48 +0200

    Add another marker to BubbleSort.

--- Algorithms/BubbleSort.py
+++ Algorithms/BubbleSort.py
@@ -7,12 +7,15 @@ class BubbleSort(SortAlgorithm):
         i = len(self.items.items)
         while changed:
             changed = False
+            bubbleMarker = self.markers.addMarker(False, 0, (255, 0, 0))
             for j in range(1, i):
                 if self.cmp.gtI(j-1, j):
                     self.items.swap(j-1, j)
                     maxChanged = j
                     changed = True
-                    yield
+                self.markers.moveMarker(bubbleMarker, j)
+                yield
+            self.markers.removeMarker(bubbleMarker)
             if changed:
                 for x in range(maxChanged, i):
                     self.markers.addMarker(False, x, (0, 255, 0))
