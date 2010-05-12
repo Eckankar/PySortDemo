a843ed4cfd08a2c068cd0d4b72dec8f8a2da0582
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-12 21:00:31 +0200

    Add markers to BidirectionalBubbleSort.

--- Algorithms/BidirectionalBubbleSort.py
+++ Algorithms/BidirectionalBubbleSort.py
@@ -10,20 +10,27 @@ class BidirectionalBubbleSort(SortAlgorithm):
         while p != r and changed:
             changed = False
             if goingDown:
+                bubbleMarker = self.markers.addMarker(False, p, (255, 0, 0))
                 for i in range(p, r):
                     if self.cmp.gtI(i, i+1):
                         self.items.swap(i, i+1)
                         changed = True
+                    self.markers.moveMarker(bubbleMarker, i+1)
                     yield
+                self.markers.addMarker(False, r, (0, 255, 0))
                 r -= 1
             else:
+                bubbleMarker = self.markers.addMarker(False, r, (255, 0, 0))
                 for i in range(r, p, -1):
                     if self.cmp.gtI(i-1, i):
                         self.items.swap(i-1, i)
                         changed = True
+                    self.markers.moveMarker(bubbleMarker, i-1)
                     yield
+                self.markers.addMarker(False, p, (0, 255, 0))
                 p += 1
 
+            self.markers.removeMarker(bubbleMarker)
             goingDown = not goingDown
             yield
 
