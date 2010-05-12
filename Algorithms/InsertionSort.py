fd5329578a39b9a385e819be8be5ee8b1eeea093
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-12 21:18:29 +0200

    Add markers to InsertionSort.

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -1,12 +1,19 @@
 from SortAlgorithm import SortAlgorithm
 
 class InsertionSort(SortAlgorithm):
-    def sort(self): 
+    def sort(self):
         yield
+        iMarker = self.markers.addMarker(True, 0, (255, 0, 0))
         for i in range(1, len(self.items.items)):
-            j = i - 1
-            while self.cmp.gtI(j, j+1) and self.cmp.gte(j, 0):
-                self.items.swap(j, j+1)
-                j -= 1
-                yield
+            self.markers.moveMarker(iMarker, i+1)
             yield
+            j = i - 1
+            jMarker = self.markers.addMarker(False, j, (255, 159, 0))
+            while self.cmp.gte(j, 0) and self.cmp.gtI(j, j+1):
+                self.items.swap(j, j+1)
+                yield
+                j -= 1
+                self.markers.moveMarker(jMarker, j)
+            yield
+            self.markers.removeMarker(jMarker)
+        self.markers.removeMarker(iMarker)
