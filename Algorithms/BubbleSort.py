015c92616d0073c458a1417e5ba54725fd76dd43
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-12 02:43:33 +0200

    Add markers to BubbleSort

--- Algorithms/BubbleSort.py
+++ Algorithms/BubbleSort.py
@@ -1,7 +1,7 @@
 from SortAlgorithm import SortAlgorithm
 
 class BubbleSort(SortAlgorithm):
-    def sort(self): 
+    def sort(self):
         yield
         changed = True
         i = len(self.items.items)
@@ -10,6 +10,10 @@ class BubbleSort(SortAlgorithm):
             for j in range(1, i):
                 if self.cmp.gtI(j-1, j):
                     self.items.swap(j-1, j)
+                    maxChanged = j
                     changed = True
                     yield
-            i -= 1
+            if changed:
+                for x in range(maxChanged, i):
+                    self.markers.addMarker(False, x, (0, 255, 0))
+                i = maxChanged
