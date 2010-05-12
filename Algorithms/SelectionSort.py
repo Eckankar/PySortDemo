0f7bd73f7e711c9577ed6a19950c22968e022b74
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-12 16:23:20 +0200

    Add markers to SelectionSort

--- Algorithms/SelectionSort.py
+++ Algorithms/SelectionSort.py
@@ -1,12 +1,22 @@
 from SortAlgorithm import SortAlgorithm
 
 class SelectionSort(SortAlgorithm):
-    def sort(self): 
+    def sort(self):
         yield
         for i in range(0, len(self.items.items)):
             min = i
+            jMarker = self.markers.addMarker(True, i, (255, 0, 0))
+            minMarker = self.markers.addMarker(False, i, (0, 0, 255))
             for j in range(i, len(self.items.items)):
+                self.markers.moveMarker(jMarker, j+1)
                 if self.cmp.gtI(min, j):
                     min = j
+                    self.markers.moveMarker(minMarker, j)
+                yield
+
+            self.markers.removeMarker(jMarker)
+            self.markers.removeMarker(minMarker)
+
             self.items.swap(i, min)
+            self.markers.addMarker(False, i, (0, 255, 0))
             yield
