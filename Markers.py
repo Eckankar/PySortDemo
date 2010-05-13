33e70cb1241145c5b94ff2788b9e63e1cbd12f09
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-14 00:47:09 +0200

    Add comment to Markers.

--- Markers.py
+++ Markers.py
@@ -1,9 +1,25 @@
 class Markers:
+    """
+    Keeps track of markers (colored lines) we want to display to make the
+    visualizations more appealing.
+    """
     def __init__(self):
         self.markers = {}
         self.id = 0
 
     def addMarker(self, above, index, color):
+        """
+        Adds a marker to our collection.
+        
+        Parameters:
+        - above (boolean): If true, the marker will be displayed above the line of the given index.
+                           If false, the marker will be displayed on the line of the given index.
+        - index (int): Index of the marker the marker should be displayed near.
+        - color (color): The color the marker is to have on the display.
+                         Should be passed as a RGB-tuple; e.g. (255, 0, 0) = red.
+
+        Returns an the id of the marker created, to be used for modifying/removing the marker at a later point in time.
+        """
         self.id += 1
 
         marker = { 'id': self.id,
@@ -16,9 +32,11 @@ class Markers:
         return self.id
 
     def moveMarker(self, id, index):
+        """ Moves the marker with the given id to the given index. """
         self.markers[id]['index'] = index
 
     def removeMarker(self, id):
+        """ Removes the marker with the given id. """
         del self.markers[id]
 
 
