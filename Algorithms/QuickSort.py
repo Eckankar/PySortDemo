e8c8d2410dcd6071c34ad6d201c82ee031d2213b
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2010-05-17 13:07:35 +0200

    Let's show what has been sorted so far in quicksort.

--- Algorithms/QuickSort.py
+++ Algorithms/QuickSort.py
@@ -21,10 +21,13 @@ class QuickSort(SortAlgorithm):
             for x in self.partition(p, r):
                 q = x
                 yield
+            self.markers.addMarker(False, q, (0, 255, 0))
             for x in self.quicksort(p, q-1):
                 yield
             for x in self.quicksort(q+1, r):
                 yield
+        else:
+            self.markers.addMarker(False, p, (0, 255, 0))
         self.markers.removeMarker(topMarker)
         self.markers.removeMarker(bottomMarker)
 
@@ -32,7 +35,7 @@ class QuickSort(SortAlgorithm):
         i = p - 1
         iMarker = self.markers.addMarker(True, i+1, (128, 0, 128))
         jMarker = self.markers.addMarker(True, p, (0, 0, 255))
-        pivotMarker = self.markers.addMarker(False, r, (0, 255, 0))
+        pivotMarker = self.markers.addMarker(False, r, (219, 110, 255))
         for j in range(p, r):
             self.markers.moveMarker(jMarker, j)
             if self.cmp.lteI(j, r):
