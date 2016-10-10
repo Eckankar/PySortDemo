4ab01df24513b568bc8bb5fba2d4dfe5a219ee86
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2016-10-10 19:09:05 +0200

    Minor changes to in-place merge sort
    * Changes to visualization to make it more along what I like.
    * Use swaps instead of assignments.

--- Algorithms/InplaceMergeSort.py
+++ Algorithms/InplaceMergeSort.py
@@ -5,7 +5,8 @@ class InplaceMergeSort(SortAlgorithm):
     """
     Implements in-place merge sort.
     O(n lg n), stable, in-place
-    http://www.cs.ubc.ca/~harrison/Java/MergeSortAlgorithm.java.html    """
+    http://www.cs.ubc.ca/~harrison/Java/MergeSortAlgorithm.java.html
+    """
 
     def sort(self):
         yield
@@ -15,7 +16,8 @@ class InplaceMergeSort(SortAlgorithm):
     def inplacemergesort(self, lo, hi):
         if self.cmp.lt(lo, hi):
             mid = int(floor((lo+hi)/2))
-            split = self.markers.addMarker(True, mid+1, (255, 0, 0))
+            split = self.markers.addMarker(True, mid+1, (0, 0, 255))
+            yield
             for x in self.inplacemergesort(lo, mid):
                 yield
             for x in self.inplacemergesort(mid+1, hi):
@@ -26,20 +28,29 @@ class InplaceMergeSort(SortAlgorithm):
 
             end_lo = mid
             start_hi = mid+1
-            loMarker = self.markers.addMarker(True, lo, (0, 0, 255))
+            loMarks = []
+            loMarker = self.markers.addMarker(False, lo, (0, 255, 0))
+            hiMarker = self.markers.addMarker(False, start_hi, (255, 0, 0))
+
             while (lo <= end_lo and start_hi <= hi):
                 self.markers.moveMarker(loMarker, lo)
+                self.markers.moveMarker(hiMarker, start_hi)
                 yield
-                if self.cmp.lte(self.items.items[lo],self.items.items[start_hi]):
+
+                if self.cmp.lteI(lo,start_hi):
+                    loMarks.append( self.markers.addMarker(False, lo, (0, 200, 0)) )
                     lo += 1
                     yield
                 else:
-                    temp = self.items.items[start_hi]
                     for k in range(start_hi-1,lo-1,-1):
-                        self.items.items[k+1] = self.items.items[k]
-                    self.items.items[lo] = temp
-                    yield
+                        self.items.swap(k, k+1)
+                        self.markers.moveMarker(hiMarker, k)
+                        yield
+                    loMarks.append( self.markers.addMarker(False, lo, (0, 200, 0)) )
                     lo += 1
                     end_lo += 1
                     start_hi += 1
+            for mark in loMarks:
+                self.markers.removeMarker(mark)
             self.markers.removeMarker(loMarker)
+            self.markers.removeMarker(hiMarker)
