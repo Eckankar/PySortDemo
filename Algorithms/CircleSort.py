ddf4941d912e7f510de3bf20b21b3f89bcec8c7f
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2017-10-18 20:35:30 +0200

    Minor tweaks to CircleSort

--- Algorithms/CircleSort.py
+++ Algorithms/CircleSort.py
@@ -1,28 +1,25 @@
 from SortAlgorithm import SortAlgorithm
 
 
-class CircleSort(SortAlgorithm): 
+class CircleSort(SortAlgorithm):
     """
     Implements circle sort.
-    O(n lg n)
-    http://www.geeksforgeeks.org/circle-sort/
+    https://sourceforge.net/p/forth-4th/wiki/Circle%20sort/
     """
 
-    swaps = 1
-
     def sort(self):
-        while self.swaps:
-            self.swaps = 0
+        self.has_swapped = True
+        while self.has_swapped:
+            self.has_swapped = False
             yield
             for x in self.CircleSort(0, len(self.items.items) - 1):
                 yield
-            self.markers.removeAllMarkers()
 
     def CircleSort(self, p, r):
         if self.cmp.eq(p, r):
             return
-        pMarker = self.markers.addMarker(False, p, (0, 0, 255))
-        rMarker = self.markers.addMarker(False, r, (0, 0, 255))
+        pMarker = self.markers.addMarker(False, p, (0, 255, 0))
+        rMarker = self.markers.addMarker(False, r, (0, 255, 0))
         yield
         high = r
         low = p
@@ -30,7 +27,7 @@ class CircleSort(SortAlgorithm):
         while self.cmp.lt(p, r):
             if self.cmp.gtI(p, r):
                 self.items.swap(p, r)
-                self.swaps += 1
+                self.has_swapped = True
             p += 1
             r -= 1
             self.markers.moveMarker(pMarker, p)
@@ -41,7 +38,7 @@ class CircleSort(SortAlgorithm):
             self.markers.moveMarker(rMarker, r+1)
             if self.cmp.gtI(p, r+1):
                 self.items.swap(p, r+1)
-                self.swaps +=1
+                self.has_swapped = True
             yield
 
         self.markers.removeMarker(pMarker)
@@ -53,3 +50,5 @@ class CircleSort(SortAlgorithm):
             yield
         for x in self.CircleSort(low+mid+1,high):
             yield
+
+        self.markers.removeMarker(split)
